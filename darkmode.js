a6468b1204e5fb9603a3ff02d60133e6857ab10a
Abdulmohsen Alshawarib <124638908+M7snitor@users.noreply.github.com>
2025-02-07 02:57:07 +0300

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,27 @@
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const rizzSound = new Audio('rizz.mp3');
+
+const acksSound = new Audio('ack.mp3');
+acksSound.volume = 0.5;
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+    rizzSound.play();
+
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+    acksSound.play();
+}
+
+if (darkmode === "active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode');
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
