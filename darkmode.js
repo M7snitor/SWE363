6feceeaeb25acc7ffd8a949ba6cad707cab1d65e
Abdulmohsen Alshawarib <124638908+M7snitor@users.noreply.github.com>
2025-02-10 19:23:22 +0300

    fixed volume

--- darkmode.js
+++ darkmode.js
@@ -5,16 +5,17 @@ const darkSound = new Audio('undertaker.mp3');
 const lightSound = new Audio('rizz.mp3');
 // const acksSound = new Audio('ack.mp3');
 // acksSound.volume = 0.5;
-
 const enableDarkmode = () => {
     document.body.classList.add('darkmode')
     localStorage.setItem('darkmode', 'active')
+    darkSound.volume = 0.4;
     darkSound.play();
 }
 
 const disableDarkmode = () => {
     document.body.classList.remove('darkmode')
     localStorage.setItem('darkmode', null)
+    lightSound.volume = 0.4;
     lightSound.play();
 }
 
